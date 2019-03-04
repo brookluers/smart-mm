5943e65567b52c70a26aa9a70da960790530c929
Brook Luers <brook@dhcp-22-203.private.isr.umich.edu>
2019-03-04 16:34:09 -0500

    use contrast with max effect size

--- res-fmt.R
+++ res-fmt.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(stringr)
-
-simnames <- c('sim1','sim2')
-effsizes <- c('small','med','large')
-ssizes <- c(50,200,1000,5000) # unique(as.numeric(str_match(allfiles, '(N)([0-9]+)')[,3]))
+library(purrr)
+simnames <- c('sim1','sim2', 'sim3')
+# effsizes <- c('small','med','large')
+effsizes <- c('small', 'large')
+ssizes <- c(50, 100)
 conflvl <- 0.95
 zmult <- qnorm((1-conflvl) / 2,lower.tail=F)
 res_objnames <- setNames(vector('character', length(simnames)), simnames)
@@ -11,6 +12,12 @@ allfiles <- NULL
 
 
 sim_effsizes <-
+  setNames(lapply(simnames, function(ss)
+    return(setNames(
+      vector('list', length(effsizes)), effsizes
+    ))),
+    simnames)
+sim_alphas <-
   setNames(lapply(simnames, function(ss)
     return(setNames(
       vector('list', length(effsizes)), effsizes
@@ -34,6 +41,7 @@ saveobj <-
     'zmult',
     'allfiles',
     'sim_effsizes',
+    'sim_alphas',
     'sim_params')
 
 for (cursim in simnames) {
@@ -45,6 +53,7 @@ for (cursim in simnames) {
   saveobj <- c(saveobj, res_objnames[cursim])
   for (ncur in ssizes) {
     nfnames <- cfiles[str_detect(cfiles, paste('N', ncur, '-', sep=''))]
+    if (length(nfnames) == 0) next
     allres <-
       vector('list', length(nfnames)) ## a list of the results for each effect size, fixed sample size
     avg_trueVs <- vector('list', length(nfnames))
@@ -53,9 +62,10 @@ for (cursim in simnames) {
       
       ni <- length(simparm$tvec)
       
-      vhatnames <- paste('v',
+      vhatnames <- matrix(paste('v',
                          paste(row(matrix('',nrow=ni,ncol=ni)),
-                               col(matrix('',nrow=ni,ncol=ni)), sep=''),sep='')
+                               col(matrix('',nrow=ni,ncol=ni)), sep=''),sep=''),
+                         nrow=ni,ncol=ni)[lower.tri(matrix('',nrow=ni,ncol=ni),diag=T)]
       cur_effsize <- str_extract(nfnames[i], 'small|med|large')
       
       avg_trueVs[[i]] <- matrix(0, nrow=ni, ncol=ni)
@@ -64,10 +74,11 @@ for (cursim in simnames) {
         avg_trueVs[[i]] <- avg_trueVs[[i]] + (1 / nregime) * simparm$Vtruelist[[cur_effsize]][[cregime]]
       }
       
-      sim_effsizes[[cursim]][[cur_effsize]] <- get(paste(cur_effsize, '_effsizes', sep=''))
+      sim_effsizes[[cursim]][[cur_effsize]] <- max(simparm$effsizelist[[cur_effsize]][6,])
+      sim_alphas[[cursim]][[cur_effsize]] <- simparm$alphalist[[cur_effsize]]
       allres[[i]] <-
-        list(truecoefs = get(paste("truecoefs_", cur_effsize, sep = '')),
-             res = get(paste("res_", cur_effsize, sep = '')))
+        list(truecoefs = simparm$truecoefs[[cur_effsize]],
+             res = res)
       names(allres)[i] <- cur_effsize
       names(avg_trueVs)[i] <- cur_effsize
       sim_params[[cursim]][[cur_effsize]][[as.character(simparm$N)]] <- simparm
@@ -81,7 +92,7 @@ for (cursim in simnames) {
           Y = -99,
           time = tmax,
           A1 = 1,
-          A2 = 1,
+          A2 = -1,
           knot = simparm$knot,
           X = simparm$X[1]
         )
@@ -93,7 +104,7 @@ for (cursim in simnames) {
           Y = -99,
           time = tmax,
           A1 = -1,
-          A2 = 1,
+          A2 = -1,
           knot = simparm$knot,
           X = simparm$X[1]
         )
@@ -101,6 +112,7 @@ for (cursim in simnames) {
     
     allres_fmt <- setNames(vector('list', length=length(effsizes)), effsizes)
     for(ceffsize in effsizes){ # for each of the effect sizes at a given sample size
+      if (is.null(allres[[ceffsize]])) next
       x <- allres[[ceffsize]]
       true_eos_difference <-
         Lmat1 %*% x$truecoefs - Lmat2 %*% x$truecoefs
@@ -134,11 +146,12 @@ for (cursim in simnames) {
           ##
           ## Get the estimates of the marginal variance matrix
           ##
+          avg_vhat <- (1/nregime) * reduce(sim_i$vhatmat,`+`)
           vhat_results <-
             data.frame(
-              coef = rep(vhatnames, each=nrow(sim_i$vhatmat)),
-              est = as.numeric(sim_i$vhatmat),
-              trueval = rep(as.numeric(avg_trueVs[[ceffsize]]), each = nrow(sim_i$vhatmat)),
+              coef = rep(vhatnames, each=nrow(sim_i$bhat)),
+              est = as.numeric(avg_vhat),
+              trueval = rep(avg_trueVs[[ceffsize]][lower.tri(avg_trueVs[[ceffsize]],diag=T)], each = nrow(sim_i$bhat)),
               N = ncur, method = rep(sim_i$method, length(vhatnames))
             )
           
